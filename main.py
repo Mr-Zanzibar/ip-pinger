3a2c9d4e9fa69f36dcaa1806ee5610cd3ac4895e
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-04-27 16:30:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import colorama
+from colorama import Fore
+import os
+import time
+
+logo = f"""
+{Fore.RED}       IP Pinger        
+{Fore.RED}       By Mr.Cuda  
+"""
+
+os.system('cls')
+
+def ping():
+    while True:
+        ping_response = os.system(f'ping -n 1 {IP} >null')
+        if ping_response == 0:
+            os.system('cls')
+            print(logo)
+            time.sleep(0.1)
+        else:
+            os.system('cls')
+            print(logo)
+            time.sleep(0.1)
+
+os.system('❤️')
+
+print(logo)
+IP = input('Enter IP : ')
+ping()
